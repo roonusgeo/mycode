bc36b04c8b7d7ff5024fbe3f3f43e6ac13637b95
isabella17272 <isabellasalazar17@gmail.com>
2023-01-12 22:33:52 +0000

    whathowgartshouseareyou.py

---
+++ farm.py
@@ -0,0 +1,26 @@
+#!usr/bin/env python3
+
+farms = [{"name": "NE Farm", "agriculture": ["sheep", "cows", "pigs", "chickens", "llamas", "cats"]},
+         {"name": "W Farm", "agriculture": ["pigs", "chickens", "llamas"]},
+         {"name": "SE Farm", "agriculture": ["chickens", "carrots", "celery"]}]
+
+NE_animals= farms[0]["agriculture"]
+
+    for x in NE_animals:
+         print(["sheep", "cows", "pigs", "chicken", "llamas", "cats"])
+
+question1= {"question": "Choose a farm"
+            "answer": ["NE Farm",
+                       "W Farm",
+                       "SE Farm",]
+while True:
+    print(question1["question"])
+
+    answer_list= list(question1["answer"])
+
+    print(answer_list)
+
+    answer= input(">>") n/ 
+
+
+
---
+++ simpsons.py
@@ -0,0 +1,20 @@
+#! usr/bin/env/ python3
+
+challenge= ["science", "turbo", ["goggles", "eyes"], "nothing"]
+
+for answer in challenge:
+    print(f'My {challenge[2][1]}! The {challenge[2][0]} do {challenge[3]}!')
+
+
+trial= ["science", "turbo", {"eyes": "goggles", "goggles": "eyes"}, "nothing"]
+
+for answer in trail:
+    print(f'My {trail[2][1]}! The {challenge[2][0]} do {challenge[3]}!')
+
+
+nightmare= [{"slappy": "a", "text": "b", "kumquat": "goggles", "user":{"awesome": "c", "name": {"first": "eyes", "last": "toes"}},"banana": 15, "d": "nothing"}]
+
+for answer in challenge:
+    print(f'My {challenge[2][1]}! The {challenge[2][0]} do {challenge[3]}!')
+
+### incomplete 
